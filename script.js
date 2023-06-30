56fc9478d559d6ac0f8988a92ef2711734ced5f6
sam <121357063+sam-sol7@users.noreply.github.com>
2023-06-30 23:27:39 +0000

    Add decoding functionality

--- script.js
+++ script.js
@@ -8,28 +8,53 @@ let isItEncode = true;
 submitButton.onclick = function () { 
     cipherText.value = ""; 
 
-    for (let letter in normalText.value) {
-        if (!alphabet.includes(normalText.value[letter].toLowerCase())) {
-            cipherText.value += normalText.value[letter];
-            continue;
-        }
-        let alphabetIndex = alphabet.indexOf(normalText.value[letter].toLowerCase());
-        let isItLowerCase = (normalText.value[letter] == normalText.value[letter].toLowerCase()) ? true : false; 
-        let cipherIndex = parseInt(indexInput.value) + alphabetIndex;
-        
-        if (cipherIndex < 0) {
-            if (isItLowerCase) cipherText.value += alphabet[ (((cipherIndex % 26) + 26) % 26) ];
-            else cipherText.value += alphabet[ (((cipherIndex % 26) + 26) % 26) ].toUpperCase();
-            continue;
-        }
-        if (cipherIndex > 25) {
-            if (isItLowerCase) cipherText.value += alphabet[cipherIndex % 26];
-            else cipherText.value += alphabet[cipherIndex % 26].toUpperCase();
-            continue;
-        }
-        if (isItLowerCase) cipherText.value += alphabet[cipherIndex];
-        else cipherText.value += alphabet[cipherIndex].toUpperCase();
-    } 
+    if (isItEncode) {
+        for (let letter in normalText.value) {
+            if (!alphabet.includes(normalText.value[letter].toLowerCase())) {
+                cipherText.value += normalText.value[letter];
+                continue;
+            }
+            let alphabetIndex = alphabet.indexOf(normalText.value[letter].toLowerCase());
+            let isItLowerCase = (normalText.value[letter] == normalText.value[letter].toLowerCase()) ? true : false; 
+            let cipherIndex = parseInt(indexInput.value) + alphabetIndex;
+            
+            if (cipherIndex < 0) {
+                if (isItLowerCase) cipherText.value += alphabet[ (((cipherIndex % 26) + 26) % 26) ];
+                else cipherText.value += alphabet[ (((cipherIndex % 26) + 26) % 26) ].toUpperCase();
+                continue;
+            }
+            if (cipherIndex > 25) {
+                if (isItLowerCase) cipherText.value += alphabet[cipherIndex % 26];
+                else cipherText.value += alphabet[cipherIndex % 26].toUpperCase();
+                continue;
+            }
+            if (isItLowerCase) cipherText.value += alphabet[cipherIndex];
+            else cipherText.value += alphabet[cipherIndex].toUpperCase();
+        } 
+    } else {
+        for (let letter in normalText.value) {
+            if (!alphabet.includes(normalText.value[letter].toLowerCase())) {
+                cipherText.value += normalText.value[letter];
+                continue;
+            }
+            let alphabetIndex = alphabet.indexOf(normalText.value[letter].toLowerCase());
+            let isItLowerCase = (normalText.value[letter] == normalText.value[letter].toLowerCase()) ? true : false; 
+            let cipherIndex = alphabetIndex - parseInt(indexInput.value);
+            
+            if (cipherIndex < 0) {
+                if (isItLowerCase) cipherText.value += alphabet[ (((cipherIndex % 26) + 26) % 26) ];
+                else cipherText.value += alphabet[ (((cipherIndex % 26) + 26) % 26) ].toUpperCase();
+                continue;
+            }
+            if (cipherIndex > 25) {
+                if (isItLowerCase) cipherText.value += alphabet[cipherIndex - (((cipherIndex % 26) + 26) % 26) ];
+                else cipherText.value += alphabet[cipherIndex - (((cipherIndex % 26) + 26) % 26) ].toUpperCase();
+                continue;
+            }
+            if (isItLowerCase) cipherText.value += alphabet[cipherIndex];
+            else cipherText.value += alphabet[cipherIndex].toUpperCase();
+        } 
+    }
 }
 
 let copyButton =  document.getElementById("copy-button");
